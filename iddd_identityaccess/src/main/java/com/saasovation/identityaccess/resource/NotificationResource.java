4d90b8a5a023b2277f82b0bb8ec79dafeac59cec
Pietro Di Bello <pierodibello@gmail.com>
2022-02-25 23:42:33 +0100

    simplify formatting

--- iddd_identityaccess/src/main/java/com/saasovation/identityaccess/resource/NotificationResource.java
+++ iddd_identityaccess/src/main/java/com/saasovation/identityaccess/resource/NotificationResource.java
@@ -14,21 +14,17 @@
 
 package com.saasovation.identityaccess.resource;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.core.UriInfo;
-
 import com.saasovation.common.media.Link;
 import com.saasovation.common.media.OvationsMediaType;
 import com.saasovation.common.notification.NotificationLog;
 import com.saasovation.common.serializer.ObjectSerializer;
 import com.saasovation.identityaccess.application.representation.NotificationLogRepresentation;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/notifications")
 public class NotificationResource extends AbstractResource {
@@ -38,101 +34,61 @@ public class NotificationResource extends AbstractResource {
     }
 
     @GET
-    @Produces({ OvationsMediaType.ID_OVATION_TYPE })
-    public Response getCurrentNotificationLog(
-            @Context UriInfo aUriInfo) {
-
-        NotificationLog currentNotificationLog =
-            this.notificationApplicationService()
-                .currentNotificationLog();
-
+    @Produces({OvationsMediaType.ID_OVATION_TYPE})
+    public Response getCurrentNotificationLog(@Context UriInfo aUriInfo) {
+        NotificationLog currentNotificationLog = notificationApplicationService().currentNotificationLog();
         if (currentNotificationLog == null) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
 
-        Response response =
-            this.currentNotificationLogResponse(
-                    currentNotificationLog,
-                    aUriInfo);
-
-        return response;
+        return currentNotificationLogResponse(currentNotificationLog, aUriInfo);
     }
 
     @GET
     @Path("{notificationId}")
-    @Produces({ OvationsMediaType.ID_OVATION_TYPE })
-    public Response getNotificationLog(
-            @PathParam("notificationId") String aNotificationId,
-            @Context UriInfo aUriInfo) {
-
-        NotificationLog notificationLog =
-            this.notificationApplicationService()
-                .notificationLog(aNotificationId);
-
+    @Produces({OvationsMediaType.ID_OVATION_TYPE})
+    public Response getNotificationLog(@PathParam("notificationId") String aNotificationId, @Context UriInfo aUriInfo) {
+        NotificationLog notificationLog = notificationApplicationService().notificationLog(aNotificationId);
         if (notificationLog == null) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
 
-        Response response =
-            this.notificationLogResponse(
-                    notificationLog,
-                    aUriInfo);
-
-        return response;
+        return notificationLogResponse(notificationLog, aUriInfo);
     }
 
-    private Response currentNotificationLogResponse(
-            NotificationLog aCurrentNotificationLog,
-            UriInfo aUriInfo) {
+    private Response currentNotificationLogResponse(NotificationLog aCurrentNotificationLog, UriInfo aUriInfo) {
 
-        NotificationLogRepresentation log =
-            new NotificationLogRepresentation(aCurrentNotificationLog);
-
-        log.setLinkSelf(
-                this.selfLink(aCurrentNotificationLog, aUriInfo));
-
-        log.setLinkPrevious(
-            this.previousLink(aCurrentNotificationLog, aUriInfo));
+        NotificationLogRepresentation log = new NotificationLogRepresentation(aCurrentNotificationLog);
+        log.setLinkSelf(selfLink(aCurrentNotificationLog, aUriInfo));
+        log.setLinkPrevious(previousLink(aCurrentNotificationLog, aUriInfo));
 
         String serializedLog = ObjectSerializer.instance().serialize(log);
 
-        Response response =
-            Response
-                .ok(serializedLog)
-                .cacheControl(this.cacheControlFor(60))
-                .build();
-
-        return response;
+        return Response
+            .ok(serializedLog)
+            .cacheControl(cacheControlFor(60))
+            .build();
     }
 
-    private Response notificationLogResponse(
-            NotificationLog aNotificationLog,
-            UriInfo aUriInfo) {
+    private Response notificationLogResponse(NotificationLog aNotificationLog, UriInfo aUriInfo) {
 
-        NotificationLogRepresentation log =
-            new NotificationLogRepresentation(aNotificationLog);
-
-        log.setLinkSelf(this.selfLink(aNotificationLog, aUriInfo));
-
-        log.setLinkNext(this.nextLink(aNotificationLog, aUriInfo));
-
-        log.setLinkPrevious(this.previousLink(aNotificationLog, aUriInfo));
+        NotificationLogRepresentation log = new NotificationLogRepresentation(aNotificationLog);
+        log.setLinkSelf(selfLink(aNotificationLog, aUriInfo));
+        log.setLinkNext(nextLink(aNotificationLog, aUriInfo));
+        log.setLinkPrevious(previousLink(aNotificationLog, aUriInfo));
 
         String serializedLog = ObjectSerializer.instance().serialize(log);
 
-        Response response =
-            Response
-                .ok(serializedLog)
-                .cacheControl(this.cacheControlFor(3600))
-                .build();
-
-        return response;
+        return Response
+            .ok(serializedLog)
+            .cacheControl(cacheControlFor(3600))
+            .build();
     }
 
     private Link linkFor(
-            String aRelationship,
-            String anId,
-            UriInfo aUriInfo) {
+        String aRelationship,
+        String anId,
+        UriInfo aUriInfo) {
 
         Link link = null;
 
@@ -148,43 +104,43 @@ public class NotificationResource extends AbstractResource {
                     .toString();
 
             link = new Link(
-                    linkUrl,
-                    aRelationship,
-                    null,
-                    OvationsMediaType.ID_OVATION_TYPE);
+                linkUrl,
+                aRelationship,
+                null,
+                OvationsMediaType.ID_OVATION_TYPE);
         }
 
         return link;
     }
 
     private Link nextLink(
-            NotificationLog aNotificationLog,
-            UriInfo aUriInfo) {
+        NotificationLog aNotificationLog,
+        UriInfo aUriInfo) {
         return
             this.linkFor(
-                    "next",
-                    aNotificationLog.nextNotificationLogId(),
-                    aUriInfo);
+                "next",
+                aNotificationLog.nextNotificationLogId(),
+                aUriInfo);
     }
 
     private Link previousLink(
-            NotificationLog aNotificationLog,
-            UriInfo aUriInfo) {
+        NotificationLog aNotificationLog,
+        UriInfo aUriInfo) {
 
         return
             this.linkFor(
-                    "previous",
-                    aNotificationLog.previousNotificationLogId(),
-                    aUriInfo);
+                "previous",
+                aNotificationLog.previousNotificationLogId(),
+                aUriInfo);
     }
 
     private Link selfLink(
-            NotificationLog aNotificationLog,
-            UriInfo aUriInfo) {
+        NotificationLog aNotificationLog,
+        UriInfo aUriInfo) {
         return
             this.linkFor(
-                    "self",
-                    aNotificationLog.notificationLogId(),
-                    aUriInfo);
+                "self",
+                aNotificationLog.notificationLogId(),
+                aUriInfo);
     }
 }
